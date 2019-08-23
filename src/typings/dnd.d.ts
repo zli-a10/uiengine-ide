fb4e1c4ab303ad322c8faf280ab43061262c8ea9
zuopingli <studytime@163.com>
2019-08-23 21:08:58 +0800

    fixed typescript errors

--- src/typings/dnd.d.ts
+++ src/typings/dnd.d.ts
@@ -1,3 +1,52 @@
-import { ILayoutSchema } from "uiengine/typings";
+interface IIDEContext {
+  preview: boolean;
+}
 
-// import { ILayoutSchema } from "uiengine/typings";
+interface IIDEEditor {
+  children?: ReactNode;
+  layouts: [string];
+  config: object;
+  manangerProps: {
+    getDataSource?: () => any;
+    expandDataSource?: (uiPath: string) => any;
+  };
+}
+
+interface IPropManager {
+  onClose: () => any;
+}
+
+interface IManager {
+  onClose: () => any;
+  getDataSource?: () => any;
+  expandDataSource?: (uiPath: string) => any;
+}
+
+interface IWidgets {
+  widgets: IWidget[];
+}
+
+interface ILayoutManager {
+  layout: string;
+}
+
+interface ITree {
+  tree: any;
+}
+
+interface ITreeState {
+  expandKeys: string[];
+  date: number;
+}
+
+interface IComponents {
+  list: any;
+}
+
+interface IWidget {
+  widget: any;
+}
+
+interface IMenuTitle {
+  dataRef: any;
+}
