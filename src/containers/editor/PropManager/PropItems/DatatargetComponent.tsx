fdadad6638f5e582c450c7f762ece91947c850c4
Rui <eneblue@126.com>
2019-11-05 16:42:24 +0800

    Update datasource

--- src/containers/editor/PropManager/PropItems/DatatargetComponent.tsx
+++ src/containers/editor/PropManager/PropItems/DatatargetComponent.tsx
@@ -1,87 +1,92 @@
-import React, { useState, useCallback, useContext, useEffect } from "react";
-import _ from "lodash";
-import { Form, List, Button, TreeSelect, Switch, Select } from "antd";
+import React, { useState, useCallback, useContext, useEffect } from 'react'
+import _ from 'lodash'
+import { Form, List, Button, TreeSelect, Switch, Select } from 'antd'
 import {
   getDataSourceDomains,
   getDataSourceFields,
   convertNodes
-} from "../../../../helpers";
-import { GlobalContext } from "../../../Context";
+} from '../../../../helpers'
+import { GlobalContext } from '../../../Context'
+import {
+  analysisDataSourceFields,
+  analysisDataSource
+} from '../../../../helpers/DataSourceLoader'
+import { IObject } from 'uiengine/typings'
 
 const TargetItem = (props: any) => {
-  const { datasource: { getDataSource } = {} as any } = useContext(
-    GlobalContext
-  );
-  const { item, onChange, index, onRemoveItem } = props;
+  const data = useContext(GlobalContext)
+  const { resourceTree: { datasource = [] } = {} } = data
 
-  const [dependOnList, setDependOnList] = useState();
-  const [schemaLineage, setSchemaLineage] = useState();
-  const [schemaMethod, setSchemaMethod] = useState();
-  const [dataSource, setDataSource] = useState();
-  const [excludes, setExcludes] = useState();
+  const { item, onChange, index, onRemoveItem } = props
 
-  const treeData = getDataSourceDomains();
-  const treeFields = getDataSourceFields();
+  const [dependOnList, setDependOnList] = useState()
+  const [schemaLineage, setSchemaLineage] = useState()
+  const [schemaMethod, setSchemaMethod] = useState()
+  const [dataSource, setDataSource] = useState()
+  const [excludes, setExcludes] = useState()
+
+  const treeData = getDataSourceDomains()
+  const treeFields = getDataSourceFields()
   const changeItem = useCallback(
     (source: string, setStateFunc: any) => {
       return (value: string) => {
-        _.set(item, source, value);
-        setStateFunc(value);
-        onChange(item, index);
-      };
+        _.set(item, source, value)
+        setStateFunc(value)
+        onChange(item, index)
+      }
     },
     [item]
-  );
+  )
 
   const onChangeDependOn = useCallback(
     (value: any, options: any) => {
-      _.set(item, "dependOn.dependList", value);
-      setDependOnList(value);
+      _.set(item, 'dependOn.dependList', value)
+      setDependOnList(value)
       // TO Enhance: multiple targets depend
-      const key = _.get(options, "0.key");
+      const key = _.get(options, '0.key')
       if (key) {
-        _.set(item, "dependOn.targetKey", key);
+        _.set(item, 'dependOn.targetKey', key)
       }
-      onChange(item, index);
+      onChange(item, index)
     },
     [item]
-  );
+  )
 
-  const [schemas, setSchemas] = useState([]);
+  const [schemas, setSchemas] = useState([] as IObject[])
   useEffect(() => {
     const initDataSource = async () => {
-      let newNodes = (await getDataSource("")) || [];
-      newNodes = convertNodes(newNodes, [], true);
-      setSchemas(newNodes);
-    };
-    initDataSource();
-  }, []);
+      let newNodes = (await analysisDataSource(datasource)) || ([] as IObject[])
+      newNodes = convertNodes(newNodes, [] as IObject[], true)
+      setSchemas(newNodes)
+    }
+    initDataSource()
+  }, [datasource])
 
   useEffect(() => {
-    const dataSource = _.get(item, "dataSource.source");
-    setDataSource(dataSource);
+    const dataSource = _.get(item, 'dataSource.source')
+    setDataSource(dataSource)
 
-    const excludes = _.get(item, "dataSource.exclude");
-    setExcludes(excludes);
+    const excludes = _.get(item, 'dataSource.exclude')
+    setExcludes(excludes)
 
-    const schemaMethod = _.get(item, "dataSchema.method");
-    setSchemaMethod(schemaMethod);
+    const schemaMethod = _.get(item, 'dataSchema.method')
+    setSchemaMethod(schemaMethod)
 
-    const schemaLineage = _.get(item, "dataSchema.lineage");
-    setSchemaLineage(schemaLineage);
+    const schemaLineage = _.get(item, 'dataSchema.lineage')
+    setSchemaLineage(schemaLineage)
 
-    const dependOn = _.get(item, "dependOn.dependList");
-    setDependOnList(dependOn);
-  }, [item]);
+    const dependOn = _.get(item, 'dependOn.dependList')
+    setDependOnList(dependOn)
+  }, [item])
 
-  const methods = ["post", "put"];
+  const methods = ['post', 'put']
 
   return (
     <List.Item key={item.key}>
       <Form.Item label="Source">
         <Select
           value={dataSource}
-          onChange={changeItem("dataSource.source", setDataSource)}
+          onChange={changeItem('dataSource.source', setDataSource)}
         >
           <Select.Option value="">None</Select.Option>
           {treeData.map((value: any, index: number) => (
@@ -96,7 +101,7 @@ const TargetItem = (props: any) => {
           <Form.Item label="Method">
             <Select
               value={schemaMethod}
-              onChange={changeItem("dataSchema.method", setSchemaMethod)}
+              onChange={changeItem('dataSchema.method', setSchemaMethod)}
             >
               <Select.Option value="">None</Select.Option>
               {methods.map((value: any, index: number) => (
@@ -110,16 +115,18 @@ const TargetItem = (props: any) => {
           <Form.Item label="Excludes">
             <Select
               value={excludes}
-              onChange={changeItem("dataSource.exclude", setExcludes)}
+              onChange={changeItem('dataSource.exclude', setExcludes)}
               mode="multiple"
             >
               <Select.Option value="">None</Select.Option>
               {treeFields[dataSource.split(':').shift()]
-                ? treeFields[dataSource.split(':').shift()].map((value: any, index: number) => (
-                  <Select.Option value={value} key={index}>
-                    {value}
-                  </Select.Option>
-                ))
+                ? treeFields[dataSource.split(':').shift()].map(
+                    (value: any, index: number) => (
+                      <Select.Option value={value} key={index}>
+                        {value}
+                      </Select.Option>
+                    )
+                  )
                 : null}
             </Select>
           </Form.Item>
@@ -130,7 +137,7 @@ const TargetItem = (props: any) => {
               value={schemaLineage}
               treeData={schemas}
               placeholder="Please select"
-              onChange={changeItem("dataSchema.lineage", setSchemaLineage)}
+              onChange={changeItem('dataSchema.lineage', setSchemaLineage)}
             />
           </Form.Item>
 
@@ -160,8 +167,8 @@ const TargetItem = (props: any) => {
         </Button>
       </Form.Item>
     </List.Item>
-  );
-};
+  )
+}
 
 /**
  *
@@ -189,97 +196,97 @@ will generate following schema:
 },
   */
 export const DatatargetComponent = (props: any) => {
-  const { schema, data, name, uinode, onChangeEvent, dataRef } = props;
+  const { schema, data, name, uinode, onChangeEvent, dataRef } = props
 
   // const _targets = _.get(dataRef, "defaultParams.target.targets", []);
-  const [targets, setTargets] = useState([]);
+  const [targets, setTargets] = useState([])
 
-  const [mode, setMode] = useState();
+  const [mode, setMode] = useState()
   const onChangeMode = useCallback(
     (value: boolean) => {
       if (value) {
-        _.set(dataRef, "defaultParams.target.mode", "sync");
+        _.set(dataRef, 'defaultParams.target.mode', 'sync')
       } else {
-        _.set(dataRef, "defaultParams.target.mode", "async");
+        _.set(dataRef, 'defaultParams.target.mode', 'async')
       }
-      setMode(value);
+      setMode(value)
       // _.set(dataRef, "defaultParams", ["target"]);
-      onChangeEvent(dataRef);
+      onChangeEvent(dataRef)
     },
     [targets]
-  );
+  )
 
   const onAddItem = useCallback(() => {
-    const newTargets: any = _.clone(targets);
+    const newTargets: any = _.clone(targets)
     const tpl = {
-      dataSource: "",
+      dataSource: '',
       dataSchema: {},
       dependOn: {}
-    };
-    newTargets.push(tpl);
-    setTargets(newTargets);
-  }, [targets]);
+    }
+    newTargets.push(tpl)
+    setTargets(newTargets)
+  }, [targets])
 
   const onChangeItem = useCallback(
     (item: any) => {
-      if (_.has(item, "dataSource")) {
-        const newTargets = [];
-        const _targets = _.get(dataRef, "defaultParams.target.targets", []);
-        let findedIndex = -1;
+      if (_.has(item, 'dataSource')) {
+        const newTargets = []
+        const _targets = _.get(dataRef, 'defaultParams.target.targets', [])
+        let findedIndex = -1
         if (_targets.length) {
           _targets.forEach((data: any, index: any) => {
-            if (_.has(data, "dataSource")) {
-              newTargets.push(data);
+            if (_.has(data, 'dataSource')) {
+              newTargets.push(data)
               if (data.dataSource === item.dataSource) {
-                findedIndex = index;
+                findedIndex = index
               }
             }
-          });
+          })
         }
         if (findedIndex === -1) {
-          newTargets.push(item);
+          newTargets.push(item)
         } else {
-          newTargets[findedIndex] = item;
+          newTargets[findedIndex] = item
         }
-        _.set(dataRef, "defaultParams.target.targets", newTargets);
-        onChangeEvent(dataRef);
+        _.set(dataRef, 'defaultParams.target.targets', newTargets)
+        onChangeEvent(dataRef)
       }
     },
     [targets]
-  );
+  )
 
   const onRemoveItem = useCallback(
     (index: number) => {
       return () => {
-        const newTargets = _.clone(targets);
-        newTargets.splice(index, 1);
-        setTargets(newTargets);
-        let saveTargets: any = [];
+        const newTargets = _.clone(targets)
+        newTargets.splice(index, 1)
+        setTargets(newTargets)
+        let saveTargets: any = []
         newTargets.forEach((data: any, index: any) => {
-          if (_.has(data, "dataSource")) {
-            saveTargets.push(data);
+          if (_.has(data, 'dataSource')) {
+            saveTargets.push(data)
           }
-        });
-        _.set(dataRef, "defaultParams.target.targets", saveTargets);
-        onChangeEvent(dataRef);
-      };
+        })
+        _.set(dataRef, 'defaultParams.target.targets', saveTargets)
+        onChangeEvent(dataRef)
+      }
     },
     [targets]
-  );
+  )
 
   useEffect(() => {
-    const _targets = _.get(dataRef, "defaultParams.target.targets", []);
-    setTargets(_targets);
+    const _targets = _.get(dataRef, 'defaultParams.target.targets', [])
+    setTargets(_targets)
 
-    const mode = _.get(dataRef, "defaultParams.target.mode");
-    if (mode === "sync") {
-      setMode(true);
+    const mode = _.get(dataRef, 'defaultParams.target.mode')
+    if (mode === 'sync') {
+      setMode(true)
     } else {
-      setMode(false);
+      setMode(false)
     }
 
     // console.log(dataRef);
-  }, [dataRef]);
+  }, [dataRef])
 
   return (
     <div className="targets">
@@ -311,5 +318,5 @@ export const DatatargetComponent = (props: any) => {
         </Button>
       </Form.Item>
     </div>
-  );
-};
+  )
+}
