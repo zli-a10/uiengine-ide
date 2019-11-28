5a42425e8a41c323397bec1e00d66fbd05186c2e
zuopingli <studytime@163.com>
2019-11-28 13:34:41 +0800

    remove conflict

--- examples/ide/server/Commander.ts
+++ examples/ide/server/Commander.ts
@@ -1,31 +1,25 @@
-import * as commands from './commands'
-import { isFunction } from 'lodash'
+import * as commands from "./commands";
+import { isFunction } from "lodash";
 
 export default class Commander {
   static async executeCommand(command: any) {
     try {
-      const { name, options } = command
-      const callback = (commands as any)[name]
+      const { name, options } = command;
+      const callback = (commands as any)[name];
       if (isFunction(callback)) {
         // client side should have same name command
-        const value = await callback(options)
-        let result: any
-        if (typeof value !== 'string') {
-<<<<<<< HEAD
-          result = await callback(options)
+        const value = await callback(options);
+        let result: any;
+        if (typeof value !== "string") {
+          result = await callback(options);
         } else {
-          result = value
-=======
-          result = { command, value: await callback(options) }
-        } else {
-          result = { command, value }
->>>>>>> 122517598bc1947b7e0d9b1cc2cc261005436a12
+          result = value;
         }
-        return JSON.stringify(result)
+        return JSON.stringify(result);
       }
     } catch (e) {
-      console.log(e.message)
-      return
+      console.log(e.message);
+      return;
     }
   }
 }
