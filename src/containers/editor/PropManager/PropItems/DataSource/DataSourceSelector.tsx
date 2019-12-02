6e35ae82c0c540771999528b0fb22312af008ee3
zuopingli <studytime@163.com>
2019-12-02 11:00:46 +0800

    fixed datasource input delay issue, add onBlur and onPressEnter

--- src/containers/editor/PropManager/PropItems/DataSource/DataSourceSelector.tsx
+++ src/containers/editor/PropManager/PropItems/DataSource/DataSourceSelector.tsx
@@ -1,8 +1,11 @@
-import React, { useEffect, useCallback, useState, useContext } from 'react'
-import * as _ from 'lodash'
-import { TreeSelect, Input, Col, Button } from 'antd'
-import { GlobalContext } from '../../../../Context'
-import { getDatasourceFields, analysisDataSource } from '../../../../../helpers'
+import React, { useEffect, useCallback, useState, useContext } from "react";
+import * as _ from "lodash";
+import { TreeSelect, Input, Col, Button } from "antd";
+import { GlobalContext } from "../../../../Context";
+import {
+  getDatasourceFields,
+  analysisDataSource
+} from "../../../../../helpers";
 
 const renderNode = (data: any) => {
   const renderFieldNode = (item: any) => {
@@ -21,8 +24,8 @@ const renderNode = (data: any) => {
       >
         {item.children ? renderNode(item.children) : null}
       </TreeSelect.TreeNode>
-    )
-  }
+    );
+  };
 
   const renderFileNode = (item: any) => {
     return (
@@ -40,119 +43,131 @@ const renderNode = (data: any) => {
         {item.children ? (
           renderNode(item.children)
         ) : (
-            <TreeSelect.TreeNode
-              title="Loading ..."
-              value={_.uniqueId('loading')}
-              key={_.uniqueId('loading')}
-            />
-          )}
+          <TreeSelect.TreeNode
+            title="Loading ..."
+            value={_.uniqueId("loading")}
+            key={_.uniqueId("loading")}
+          />
+        )}
       </TreeSelect.TreeNode>
-    )
-  }
+    );
+  };
 
   return data.map((item: any) => {
-    if (item.type === 'file') {
-      return renderFileNode(item)
+    if (item.type === "file") {
+      return renderFileNode(item);
     }
-    if (item.type === 'field') {
-      return renderFieldNode(item)
+    if (item.type === "field") {
+      return renderFieldNode(item);
     }
-    return null
-  })
-}
+    return null;
+  });
+};
 
 // schemas fetch
 export const DataSourceSelector: React.FC<IDataSourceTreeProps> = (
   props: IDataSourceTreeProps
 ) => {
-  const { onChange, value, disabled, multipleChecked = false } = props
+  const { onChange, value, disabled, multipleChecked = false } = props;
 
-  const data = useContext(GlobalContext)
-  const { resourceTree: { datasource = [] } = {} } = data
-  const [nodes, setNodes] = useState([] as any[])
+  const data = useContext(GlobalContext);
+  const { resourceTree: { datasource = [] } = {} } = data;
+  const [nodes, setNodes] = useState([] as any[]);
 
   const onLoadData = useCallback(
     (treeNode: any) => {
-      const fileName: any = _.get(treeNode, `props.dataRef.file`)
+      const fileName: any = _.get(treeNode, `props.dataRef.file`);
       if (fileName) {
-        const fieldsPromise = getDatasourceFields(fileName)
+        const fieldsPromise = getDatasourceFields(fileName);
         fieldsPromise.then((data: any) => {
-          let children = _.get(treeNode, `props.dataRef.children`)
+          let children = _.get(treeNode, `props.dataRef.children`);
           if (children && _.isArray(data)) {
-            children = _.concat(children, data)
+            children = _.concat(children, data);
           } else {
-            children = data
+            children = data;
           }
-          _.set(treeNode, `props.dataRef.children`, children)
-          const newNodes = _.clone(nodes)
-          setNodes(newNodes)
-        })
-        return fieldsPromise
+          _.set(treeNode, `props.dataRef.children`, children);
+          const newNodes = _.clone(nodes);
+          setNodes(newNodes);
+        });
+        return fieldsPromise;
       }
       return new Promise((resolve: any) => {
-        resolve()
-      })
+        resolve();
+      });
     },
     [nodes]
-  )
+  );
 
-  const [showInput, setShowInput] = useState(false)
+  const [showInput, setShowInput] = useState(false);
   const switchEdit = useCallback(() => {
-    setShowInput(!showInput)
-  }, [showInput])
+    setShowInput(!showInput);
+  }, [showInput]);
 
   const onChangeValue = useCallback(
-    (value: string) => {
-      if (onChange) onChange(value)
+    (e: any) => {
+      let value = typeof e === "string" ? e : e.target.value;
+      if (onChange) onChange(value);
     },
     [onChange]
-  )
+  );
+
+  const [newValue, setNewValue] = useState(value);
+  const onInputValue = useCallback(
+    (e: any) => {
+      setNewValue(e.target.value);
+    },
+    [onChange]
+  );
 
   useEffect(() => {
     const initDataSource = async () => {
       if (nodes.length === 0 && datasource.length !== 0) {
-        const loadedNodes = (await analysisDataSource(datasource)) || []
-        setNodes(loadedNodes)
+        const loadedNodes = (await analysisDataSource(datasource)) || [];
+        setNodes(loadedNodes);
       }
+    };
+    initDataSource();
+    if (value !== newValue) {
+      setNewValue(value);
     }
-    initDataSource()
-  }, [nodes, datasource])
+  }, [nodes, datasource, value]);
 
   return (
     <div className="datasource-select">
       <Col span={20}>
         {showInput ? (
           <Input
-            {...(value ? { value } : {})}
+            value={newValue}
             disabled={disabled}
-            onChange={(e: any) => {
-              onChangeValue(e.target.value)
-            }}
+            onChange={onInputValue}
+            onBlur={onChangeValue}
+            onPressEnter={onChangeValue}
           />
         ) : (
-            <TreeSelect
-              dropdownClassName="cancel-drag"
-              disabled={disabled}
-              treeCheckable={multipleChecked}
-              allowClear
-              {...(value ? { value } : {})}
-              showSearch
-              onChange={(value: any) => onChangeValue(value)}
-              loadData={onLoadData}
-              size="small"
-            >
-              {renderNode(nodes)}
-            </TreeSelect>
-          )}
+          <TreeSelect
+            dropdownClassName="cancel-drag"
+            disabled={disabled}
+            treeCheckable={multipleChecked}
+            allowClear
+            {...(value ? { value } : {})}
+            showSearch
+            onChange={(value: any) => onChangeValue(value)}
+            loadData={onLoadData}
+            size="small"
+          >
+            {renderNode(nodes)}
+          </TreeSelect>
+        )}
       </Col>
       <Col span={4}>
         <Button
           type="primary"
           size="small"
-          icon={showInput ? 'search' : 'edit'}
+          icon={showInput ? "search" : "edit"}
           onClick={switchEdit}
         />
       </Col>
     </div>
-  )
-}
+  );
+};
